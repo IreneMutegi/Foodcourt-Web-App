e91e48f599125a77ff1b86edf40acc5c6943ef3d
rovenelabanga <rovenelabanga2001@gmail.com>
2025-02-24 21:04:43 +0300

    adds quantity and table number to cart table, adds a loading icon, changes the add to cart button, changes card styling

--- client/app/cart/page.js
+++ client/app/cart/page.js
@@ -11,6 +11,21 @@ export default function Cart() {
     setCart((prevCart) => prevCart.filter((_, i) => i !== index));
   };
 
+  const updateCartItem = (index, field, value) => {
+    setCart((prevCart) =>
+      prevCart.map((item, i) =>
+        i === index
+          ? {
+              ...item,
+              [field]: value,
+              total:
+                item.price * (field === "quantity" ? value : item.quantity),
+            }
+          : item
+      )
+    );
+  };
+
   const totalAmount = parseFloat(
     cart.reduce((sum, item) => sum + item.total, 0).toFixed(2)
   );
@@ -18,8 +33,8 @@ export default function Cart() {
     return (
       <div className="empty-cart">
         <div>
-        <FcEmptyTrash size={60} color="#4db6ac" />
-        <p>No Meals In Cart</p>
+          <FcEmptyTrash size={60} color="#4db6ac" />
+          <p>Your Cart is empty</p>
         </div>
       </div>
     );
@@ -34,6 +49,7 @@ export default function Cart() {
             <th>Meal</th>
             <th>Price</th>
             <th>Quantity</th>
+            <th>Table Number</th>
             <th>Total</th>
           </tr>
         </thead>
@@ -42,7 +58,30 @@ export default function Cart() {
             <tr key={index}>
               <td>{item.meal}</td>
               <td>{item.price}</td>
-              <td>{item.quantity}</td>
+              <td>
+                <input
+                  type="number"
+                  placeholder="Quantity"
+                  value={item.quantity}
+                  onChange={(e) =>
+                    updateCartItem(
+                      index,
+                      "quantity",
+                      parseInt(e.target.value) || 1
+                    )
+                  }
+                />
+              </td>
+              <td>
+                <input
+                  type="number"
+                  placeholder="Table Number"
+                  value={item.tableNumber}
+                  onChange={(e) =>
+                    updateCartItem(index, "tableNumber", e.target.value)
+                  }
+                />
+              </td>
               <td>{item.total}</td>
               <td>
                 <button id="remove-btn" onClick={() => removeItem(index)}>
