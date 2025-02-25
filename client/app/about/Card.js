6d01e1ff658295a428becca3d02e1792751e44f7
Irene Mutegi <irene@Ms-Echo.>
2025-02-25 22:49:06 +0300

    Update about section in the client app

---
+++ client/app/about/Card.js
@@ -0,0 +1,18 @@
+// app/about/Card.js
+import React from 'react';
+
+export function Card({ children }) {
+  return (
+    <div className="card">
+      {children}
+    </div>
+  );
+}
+
+export function CardContent({ children }) {
+  return (
+    <div className="card-content">
+      {children}
+    </div>
+  );
+}
