c1269dbf4f9eca1e543bb618c64fab0eb2c25e21
rovenelabanga <rovenelabanga2001@gmail.com>
2025-02-25 15:47:20 +0300

    styles restaurant cards

--- client/app/page.js
+++ client/app/page.js
@@ -122,12 +122,10 @@ export default function Home() {
                   <div className="image-container">
                     <img src={restaurant.image_url} alt={restaurant.name} />
                   </div>
-
                   <div className="restaurant-details">
                     <h2>{restaurant.name}</h2>
                     <p>{restaurant.cuisine}</p>
                   </div>
-
                 </div>
               ))
             ) : (
