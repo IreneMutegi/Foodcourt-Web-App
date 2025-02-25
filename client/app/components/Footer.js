aa00f3395fd7e615be350a037826fbec6e9ac29e
cullen kaberere <kabesh86@gmail.com>
2025-02-25 20:27:56 +0300

    add cart link to footer

--- client/app/components/Footer.js
+++ client/app/components/Footer.js
@@ -29,8 +29,9 @@ const Footer = () => {
           <ul className="footer-links">
             <li><Link href="/">Home</Link></li>
             <li><Link href="/about">About</Link></li>
-            <li><Link href="/restaurants">Restaurants</Link></li>
-            <li><Link href="/contact">Contact</Link></li>
+            <li><Link href="/cart">Cart</Link></li>
+            {/* <li><Link href="/restaurants">Restaurants</Link></li>
+            <li><Link href="/contact">Contact</Link></li> */}
           </ul>
         </div>
 
