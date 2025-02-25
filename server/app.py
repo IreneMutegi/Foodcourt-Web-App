88df5c645098242bfdb028ce62710f9394e44965
Irene Mutegi <irenegaceri98@gmail.com>
2025-02-26 01:46:15 +0300

    Update app.py

--- server/app.py
+++ server/app.py
@@ -3,6 +3,8 @@ from flask_cors import CORS
 from flask_restful import Api, Resource
 from flask_migrate import Migrate
 from server.models import db, Client, Admin, Restaurant, Menu, orders_association  
+from sqlalchemy import select, insert
+
 import os
 
 app = Flask(__name__)
