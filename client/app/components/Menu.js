a74d7756041b8fe3732d16ae8e70f1ed741aca8b
zippykitche <zippyk80@gmail.com>
2025-02-26 19:09:54 +0300

    added more styling

--- client/app/components/Menu.js
+++ client/app/components/Menu.js
@@ -39,7 +39,7 @@ export default function Menu({ restaurant, onClose, meals, addToCart }) {
           {meals.length > 0 ? (
             meals.map((item, index) => (
               <div key={index} className="menu-item">
-                <img src={item.image} alt={item.name} className="menu-image" />
+                <img src="https://plus.unsplash.com/premium_photo-1676973464100-beca51b27e24?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDIxfHhqUFI0aGxrQkdBfHxlbnwwfHx8fHw%3D" alt={item.name} className="menu-image" />
                 <h4>{item.name}</h4>
                 <p className="price">${item.price.toFixed(2)}</p>
                 <p className="category">{item.category}</p>
