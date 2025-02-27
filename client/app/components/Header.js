ec0b3864e99c4f006c3e7c6fc62a1f5506a3d0ab
Nattyprofessor <kabuthuantony@gmail.com>
2025-02-27 10:59:00 +0300

    good

--- client/app/components/Header.js
+++ client/app/components/Header.js
@@ -1,10 +1,11 @@
 "use client";
 import { useState, useEffect } from "react";
 import Link from "next/link";
-import { FiMenu, FiX, FiUser, FiHome, FiShoppingCart, FiShoppingBag, FiInfo, FiCalender } from "react-icons/fi";
+import { FiMenu, FiX, FiUser, FiHome, FiShoppingCart, FiShoppingBag, FiInfo} from "react-icons/fi";
 import { signOut, useSession } from "next-auth/react";
 import { usePathname } from "next/navigation";
 import OrdersPopup from "./ordersPopup";
+import { FiCalendar } from "react-icons/fi";
 import ReservationsPopup from "./reservationsPopup";
 import "./Header.css";
 
@@ -66,7 +67,7 @@ const Header = ({ setIsModalOpen }) => {
                   onClick={() => setShowOrdersPopup(true)}
                   className="nav-item orders-btn"
                 >
-                  <FiShoppingCart size={26} />
+                  { <FiShoppingCart size={26} /> }
                   <span>Orders</span>
                 </button>
               </li>
