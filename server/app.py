5506af01a86228863270c133b319aa2d6f8bbfe5
Irene Mutegi <irenegaceri98@gmail.com>
2025-02-28 00:00:19 +0300

    Update app.py wrong

--- server/app.py
+++ server/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask_restful import Api, Resource
 from flask_migrate import Migrate
-from server.models import db, Client, Admin, Restaurant, Menu, orders_association, reservation_association ,RestaurantTable
+from models import db, Client, Admin, Restaurant, Menu, orders_association, reservation_association ,RestaurantTable
 from sqlalchemy import select, delete , DateTime
 import os
 
